b35ad785ada3a48de2256ba29947a76460bc341b
BitRick <36227628+bitsRick@users.noreply.github.com>
2023-07-28 20:33:44 +1000

    edits

--- Assets/Scripts/Player/HealthBar.cs
+++ Assets/Scripts/Player/HealthBar.cs
@@ -1,27 +1,43 @@
 ﻿using System.Collections;
 using UnityEngine;
-using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 namespace Player
 {
-    public class HealthBar:MonoBehaviour
+    [RequireComponent(typeof(Player))]
+    public class HealthBar : MonoBehaviour
     {
-        [SerializeField] private Slider _sliderUIBar; 
+        [SerializeField] private Slider _sliderUIBar;
         [SerializeField] private float _fillingRateUIBar;
+        [SerializeField] private Player _player;
 
         private float _currentValue;
         private Coroutine _fillingUiBarRoutine;
 
+        private void OnEnable()
+        {
+            _player.updatedValueHealth += UpdateValue;
+        }
+
+        private void Start()
+        {
+            SetValue(_player.MaximumHealth, _player.CurrentHealth);
+        }
+
+        private void OnDisable()
+        {
+            _player.updatedValueHealth -= UpdateValue;
+        }
+
         public void UpdateValue(float value)
         {
-            if(_fillingUiBarRoutine != null)
+            if (_fillingUiBarRoutine != null)
                 StopCoroutine(_fillingUiBarRoutine);
 
             _fillingUiBarRoutine = StartCoroutine(FadeIn(value));
         }
 
-        public void SetValue(float maxValue,float currentValue)
+        private void SetValue(float maxValue, float currentValue)
         {
             _sliderUIBar.maxValue = maxValue;
             _sliderUIBar.value = currentValue;
@@ -30,13 +46,13 @@ namespace Player
         private IEnumerator FadeIn(float value)
         {
             _currentValue = value;
-            
+
             while (_sliderUIBar.value != _currentValue)
             {
                 _sliderUIBar.value = Mathf.MoveTowards(
-                _sliderUIBar.value,
-                _currentValue, 
-                Time.deltaTime * _fillingRateUIBar);
+                    _sliderUIBar.value,
+                    _currentValue,
+                    Time.deltaTime * _fillingRateUIBar);
 
                 yield return null;
             }
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,47 +1,35 @@
 using System;
 using UnityEngine;
 using UnityEngine.Events;
-using UnityEngine.Serialization;
 
 namespace Player
 {
-    [RequireComponent(typeof(HealthBar))]
     public class Player : MonoBehaviour
     {
-        private const float MinimumHealth = 0f;
-        private const float MaximumHealth = 100f;
+        public event UnityAction<float> updatedValueHealth;
 
-        [SerializeField] private HealthBar _healthBar;
-        
-        private UnityAction<float> _updatedBarValue;
-        private float _currentHealth = 50f;
-
-        private void OnEnable()
-        {
-            _updatedBarValue += _healthBar.UpdateValue;
-        }
+        public float MaximumHealth { get; private set; }
+        public float MinimumHealth { get; private set; }
+        public float CurrentHealth { get; private set; }
 
         private void Start()
         {
-            _healthBar.SetValue(MaximumHealth,_currentHealth);
+            MaximumHealth = 100f;
+            MinimumHealth = 0f;
+            CurrentHealth = 50f;
         }
 
-        private void OnDisable()
-        {
-            _updatedBarValue -= _healthBar.UpdateValue;
-        }
-
-        public void AddHealth(float addHealth = 10f) => 
+        public void AddHealth(float addHealth = 10f) =>
             UpdateHealth(addHealth);
 
-        public void AddDamage(float addDamage = 10f) => 
+        public void AddDamage(float addDamage = 10f) =>
             UpdateHealth(-addDamage);
 
         private void UpdateHealth(float updateValue)
         {
-            _currentHealth += updateValue;
-            _currentHealth = Mathf.Clamp(_currentHealth,MinimumHealth,MaximumHealth);
-            _updatedBarValue?.Invoke(_currentHealth);
+            CurrentHealth += updateValue;
+            CurrentHealth = Mathf.Clamp(CurrentHealth, MinimumHealth, MaximumHealth);
+            updatedValueHealth?.Invoke(CurrentHealth);
         }
     }
 }
